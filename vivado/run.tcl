6c2cb343729ac1dfb0734063e033d530b036b59f
Shuya Saeki <fzk03445@gmail.com>
2019-11-07 21:29:18 +0900

    add Vivado tcl script

--- vivado/run.tcl
+++ vivado/run.tcl
@@ -1,11 +1,11 @@
-create_project project_1 /tmp/uvm_example/vivado/project_1 -part xc7z020clg484-1
-add_files -norecurse /tmp/uvm_example/design/design.sv
+create_project project_1 project_1 -part xc7z020clg484-1
+add_files -norecurse ../design/design.sv
 update_compile_order -fileset sources_1
 set_property -name {xsim.compile.xvlog.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 set_property -name {xsim.elaborate.xelab.more_options} -value {-L uvm} -objects [get_filesets sim_1]
 set_property -name {xsim.simulate.runtime} -value {} -objects [get_filesets sim_1]
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
-add_files -fileset sim_1 -norecurse /tmp/uvm_example/dv/testbench.sv
+add_files -fileset sim_1 -norecurse ../dv/testbench.sv
 update_compile_order -fileset sim_1
 launch_simulation
 source test.tcl
